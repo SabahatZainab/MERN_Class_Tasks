b3279e7453e137dd845c37086348aebc939ea748
Sabahat Zainab <64367762+Smernz@users.noreply.github.com>
2023-06-19 12:35:09 +0500

    changes

--- 4/config/mongodb.js
+++ 4/config/mongodb.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
 //await is only used in async functions
 
-mongoose.connect('mongodb://localhost/my_database');
+mongoose.connect('mongodb://0.0.0.0:27017/my_database');
 try {
-    if(mongoose.connect('mongodb://localhost/my_database')){
+    if(mongoose.connect('mongodb://0.0.0.0:27017/my_database')){
     console.log('connected successfully')
     }
   } catch (error) {
     handleError(error);   
   }
+ 
+ 
+// mongoose.connect('mongodb://localhost/my_database');
+// try {
+//     if(mongoose.connect('mongodb://localhost/my_database')){
+//     console.log('connected successfully')
+//     }
+//   } catch (error) {
+//     handleError(error);   
+//   }
+// async function dbConnect()
+// {
+//     let result = await client.connect();
+//     let db = result.db(dbName);
+//     return db.collection('products');
+// }
 
-
+// module.exports = client.connect();
 
 // await MyModel.findOne();
    
@@ -25,14 +41,14 @@ try {
 // }
 // module.exports = connection; //set the enviroment variable for connection
 
-        // const Schema = mongoose.Schema;
-        // // const ObjectId = Schema.ObjectId;
+        const Schema = mongoose.Schema;
+        const ObjectId = Schema.ObjectId;
 
-        // const post = new Schema({
-        //     title: String,
-        //     description: String
+        const post = new Schema({
+            title: String,
+            description: String
 
-        // });
-// module.exports = mongoose.model('Post',post); //create model named Post and use the above post schema
+        });
+module.exports = mongoose.model('post',post); //create model named Post and use the above post schema
 
 
